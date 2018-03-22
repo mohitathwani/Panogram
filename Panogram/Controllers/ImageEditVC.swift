67a555cf1856ea00eab914535fc568e6948eef81
Mohit Athwani <mohit@athwani.net>
2018-03-21 21:33:13 -0700

    Collectionview will now scroll to center by default

--- Panogram/Controllers/ImageEditVC.swift
+++ Panogram/Controllers/ImageEditVC.swift
@@ -72,8 +72,8 @@ class ImageEditVC: UIViewController, ErrorPresenting {
 //        self.navigationController?.navigationBar.isHidden = true
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         carouselView.scrollToCenter()
     }
     
